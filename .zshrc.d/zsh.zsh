81b90eaf297daa0167b22b6d821e0da99119d334
Pedro Cavaleiro <pm.cavaleiro@hotmail.com>
2020-03-12 14:50:09 +0000

    Added create multi alias script

--- .zshrc.d/zsh.zsh
+++ .zshrc.d/zsh.zsh
@@ -4,55 +4,72 @@
 
 function zshrc {
 	case $1 in
-	"-c")
-            	if [ $# -eq 2 ]; then 
-		    	eval "nano ~/.zshrc.d/$2.zsh"
-            	else
-                	eval "nano ~/.zshrc"
-            	fi
-            	;;
+		"-c")
+            if [ $# -eq 2 ]; then 
+			    eval "nano ~/.zshrc.d/$2.zsh"
+            else
+                eval "nano ~/.zshrc"
+            fi
+            ;;
         "-r")
-		eval "source ~/.zshrc"
-            	echo "zshrc updated"
-		;;
+			eval "source ~/.zshrc"
+            echo "zshrc updated"
+			;;
         "-cr")
-            	eval "zshrc -c $2"
-            	eval "zshrc -r"
-        	;;
-	"-a")
-		if [ $# -eq 3 ]; then
-			eval 'echo "# Added at $(date)\nalias $2=\"$3\"" >> ~/.zshrc.d/alias.zsh'
-			eval "zshrc -r"
-		else
-			echo "This option requires two parameters run \"zshrc -h\"for help"
-		fi
-		;;
+            eval "zshrc -c $2"
+            eval "zshrc -r"
+            ;;
+		"-a")
+			if [ $# -eq 3 ]; then
+				eval 'echo "# Added at $(date)\nalias $2=\"$3\"" >> ~/.zshrc.d/alias.zsh'
+				eval "zshrc -r"
+			else
+				echo "This option requires two parameters run \"zshrc -h\"for help"
+			fi
+			;;
         "--create-config")
-            	if [ $# -eq 2 ]; then
-                	eval "nano ~/.zshrc.d/$2.zsh"
-            	else
-               		echo "This option requires one parameter run \"zshrc -h\"for help"
-            	fi
-        	;;
+            if [ $# -eq 2 ]; then
+                eval "nano ~/.zshrc.d/$2.zsh"
+            else
+                echo "This option requires one parameter run \"zshrc -h\"for help"
+            fi
+            ;;
+        "--multi-alias")
+            read  "?Enter the multi alias name: " multiAliasName
+            echo "# ----------------------------------------\n# $multiAliasName navigation shortcuts\n# ----------------------------------------" > $HOME/.zshrc.d/$multiAliasName.zsh
+            echo "\nfunction $multiAliasName {\n\tcase \$1 in" >> $HOME/.zshrc.d/$multiAliasName.zsh
+            exitFlag="n"
+            while [ $exitFlag = "n" ]
+            do
+                read "?Alias: " aliasName
+                read "?Path: " aliasPath
+                echo "\t\t\"$aliasName\") eval \"cd $aliasPath\"" >> $HOME/.zshrc.d/$multiAliasName.zsh
+                echo "\t\t;;" >> $HOME/.zshrc.d/$multiAliasName.zsh
+                read "?End multi alias script creation? (y/n) " exitFlag
+            done            
+            echo "\tesac\n}" >> $HOME/.zshrc.d/$multiAliasName.zsh
+            eval "zshrc -r"
+            ;;
         "-l")
-            	for cfg in ~/.zshrc.d/*.zsh; do
-                	echo ${$(basename -- "$cfg")%.*}
-            	done
-        	;;
-	"-h")
-            	echo "+------------+"
-            	echo "| zshrc help |"
-            	echo "+------------+"
-            	echo "usage: zshrc option [parameters]"
-            	echo "Option\t\tDescription"
-            	echo "-l\t\tLists all the loaded configurations"
-           	echo "-c\t\tOpens the configuration file\n\t\tIf no configuration name is passed the default \"~./zshrc\" will be opened"
-            	echo "-r\t\tReloads the zsh configurations"
-            	echo "-cr\t\tIt's the usage of -c and -r"
-            	echo "-a\t\tAdds a new alias to the alias configuration file\n\t\tIt requires two parameters <alias_name> and <command>"
-            	echo "--create-config\tCreates a new confign file"
-            	echo "-h\t\tShows the help menu"
-		;;
+            for cfg in ~/.zshrc.d/*.zsh; do
+                echo ${$(basename -- "$cfg")%.*}
+            done
+            ;;
+		"-h")
+            echo "+------------+"
+            echo "| zshrc help |"
+            echo "+------------+"
+            echo "usage: zshrc option [parameters]\n"
+            echo "Option\t\tDescription"
+            echo "-l\t\tLists all the loaded configurations"
+            echo "-c\t\tOpens the configuration file\n\t\tIf no configuration name is passed the default \"~./zshrc\" will be opened"
+            echo "-r\t\tReloads the zsh configurations"
+            echo "-cr\t\tIt's the usage of -c and -r"
+            echo "-a\t\tAdds a new alias to the alias configuration file\n\t\tIt requires two parameters <alias_name> and <command>"
+            echo "--create-config\tCreates a new confign file"
+            echo "--multi-alias\tCreates a new multi alias script"
+            echo "-h\t\tShows the help menu"
+            ;;
         *)
 		eval "zshrc -h"
 		;;
