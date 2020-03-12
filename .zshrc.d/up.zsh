769cc49e01cc6e0703a4045c7e0459919ac01e64
Unknown <thoga31@gmail.com>
2020-03-12 00:15:49 +0000

    Updates package manager script

--- .zshrc.d/up.zsh
+++ .zshrc.d/up.zsh
@@ -2,9 +2,22 @@
 # Package manager
 # ----------------------------------------
 
-alias update='sudo apt update'
-alias upgrade='sudo apt upgrade'
-alias upall='update && upgrade'
-alias install='sudo apt install'
-alias remove='sudo apt remove'
-alias autoremove='sudo apt autoremove'
+pkgmgr() {
+    if [ -z "$1" ]; then
+        pkg="apt"
+    else
+        pkg=$1
+    fi
+
+    alias update="sudo $pkg update"
+    alias upgrade="sudo $pkg upgrade"
+    alias upall="update && upgrade"
+    alias install="sudo $pkg install"
+    alias remove="sudo $pkg remove"
+    alias autoremove="sudo $pkg autoremove"
+
+    unset -v pkg
+}
+
+# Execute the pkgmgr command
+pkgmgr
