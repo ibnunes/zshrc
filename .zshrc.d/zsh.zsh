d7f906c53ec05ba244f1bdb50eb0822f41eda7f6
Igor Nunes <thoga31@gmail.com>
2023-06-28 02:48:22 +0100

    Refactors zsh.zsh configuration file
    Except the multi-alias function

--- .zshrc.d/zsh.zsh
+++ .zshrc.d/zsh.zsh
@@ -1,10 +1,9 @@
 # ---------------------------------------
-# ZSH Configuration
+# ZSHRC Configuration
 # ---------------------------------------
 
-function zshrc {
-    function zshrc_help() {
-        echo "   $(font bold fg_cyan)                                   zshrc                                    $(font reset)
+function __zshrc_help() {
+    echo "   $(font bold fg_cyan)                                   zshrc                                    $(font reset)
    ────────────────────────────────────────────────────────────────────────────
    $(font bold underline)Usage$(font reset):  $(font bg 238) zshrc option [parameters] $(font reset)
 
@@ -17,79 +16,116 @@ function zshrc {
    │ $(font bg 238) -r $(font reset)              │ Reloads zsh configurations.                          │
    │ $(font bg 238) -cr $(font reset)             │ Performs $(font bg 238) -c $(font reset) followed by $(font bg 238) -r $(font reset).                      │
    │ $(font bg 238) -a $(font reset)              │ Adds a new alias to the alias configuration file.    │
-   │                   │ It requires two parameters: $(font bg 238) alias_name $(font reset), $(font bg 238) command $(font reset). │
+   │                   │ It requires 2 parameters: $(font bg 238) alias_name $(font reset), $(font bg 238) command $(font reset).   │
    │ $(font bg 238) --create-config $(font reset) │ Creates a new configuration file.                    │
    │ $(font bg 238) --multi-alias $(font reset)   │ Creates a new multi-alias script.                    │
    │ $(font bg 238) -u $(font reset)              │ Updates the main $(font underline)zsh.zsh$(font reset) script.                     │
    │ $(font bg 238) -h $(font reset)              │ Shows this help menu.                                │
    └───────────────────└──────────────────────────────────────────────────────┘
+
+   $(font bold underline)Authors$(font reset):     Igor Nunes          https://github.com/ibnunes
+                Pedro Cavaleiro     https://github.com/PedroCavaleiro
 "
     }
 
+function __zshrc_configure() {
+    local ZSHRC_FILE="~/.zshrc"
+    [ $# -eq 1 ] && ZSHRC_FILE="$ZSHRC_FILE.d/$1.zsh"
+    echo "   Opening $ZSHRC_FILE…"
+    nano $ZSHRC_FILE
+}
+
+function __zshrc_reload() {
+    echo -n "   Reloading zsh configurations… "
+    source ~/.zshrc
+    echo "$(font fg_green)OK$(font reset)"
+}
+
+function __zshrc_alias() {
+    if [ $# -eq 2 ]; then
+        echo "   Adding new alias: \"$1\" as \"$2\"…"
+        echo "# Added at $(date)\nalias $1='$2'" >> ~/.zshrc.d/alias.zsh
+        echo "$(font fg_green)OK$(font reset)"
+        zshrc -r
+    else
+        echo "$(font fg_red)Invalid parameters.$(font reset) This option requires 2 parameters: $(font bg 238) alias_name $(font reset), $(font bg 238) command $(font reset)"
+    fi
+}
+
+function __zshrc_new_config() {
+    if [ $# -eq 1 ]; then
+        nano ~/.zshrc.d/$1.zsh
+    else
+        echo "$(font fg_red)Invalid parameters.$(font reset) This option requires 1 parameter: $(font bg 238) file_name $(font reset)."
+    fi
+}
+
+function __zshrc_multialias() {
+    return 0        # Work in progress
+
+    read  "?Enter the multi alias name: " multiAliasName
+    echo "# ----------------------------------------\n# $multiAliasName navigation shortcuts\n# ----------------------------------------" > $HOME/.zshrc.d/$multiAliasName.zsh
+    echo "\nfunction $multiAliasName {\n\tcase \$1 in" >> $HOME/.zshrc.d/$multiAliasName.zsh
+    exitFlag="n"
+    while [ $exitFlag = "n" ]
+    do
+        read "?Alias: " aliasName
+        read "?Path: " aliasPath
+        echo "\t\t\"$aliasName\") eval \"cd $aliasPath\"" >> $HOME/.zshrc.d/$multiAliasName.zsh
+        echo "\t\t;;" >> $HOME/.zshrc.d/$multiAliasName.zsh
+        read "?End multi alias script creation? (y/n) " exitFlag
+    done
+    echo "\tesac\n}" >> $HOME/.zshrc.d/$multiAliasName.zsh
+    eval "zshrc -r"
+}
+
+function __zshrc_list() {
+    for cfg in ~/.zshrc.d/*.zsh; do
+        echo ${$(basename -- "$cfg")%.*}
+    done
+}
+
+function __zshrc_update() {
+    echo -n "   Updating zsh script… "
+    curl -s https://raw.githubusercontent.com/ibnunes/zshrc/master/.zshrc.d/zsh.zsh > $HOME/.zshrc.d/zsh.zsh
+    echo "$(font fg_green)OK$(font reset)"
+    __zshrc_reload
+}
+
+function zshrc {
     case $1 in
         ("-c")
-            if [ $# -eq 2 ]; then
-                eval "nano ~/.zshrc.d/$2.zsh"
-            else
-                eval "nano ~/.zshrc"
-            fi
+            __zshrc_configure $2
             ;;
         ("-r")
-            eval "source ~/.zshrc"
-            echo "zshrc updated"
+            __zshrc_reload
             ;;
         ("-cr")
-            eval "zshrc -c $2"
-            eval "zshrc -r"
+            __zshrc_configure $2
+            __zshrc_reload
             ;;
         ("-a")
-            if [ $# -eq 3 ]; then
-                eval 'echo "# Added at $(date)\nalias $2=\"$3\"" >> ~/.zshrc.d/alias.zsh'
-                eval "zshrc -r"
-            else
-                echo "This option requires two parameters run \"zshrc -h\"for help"
-            fi
+            shift; __zshrc_alias $@
             ;;
         ("--create-config")
-            if [ $# -eq 2 ]; then
-                eval "nano ~/.zshrc.d/$2.zsh"
-            else
-                echo "This option requires one parameter run \"zshrc -h\"for help"
-            fi
+            shift; __zshrc_new_config $@
             ;;
         ("--multi-alias")
-            read  "?Enter the multi alias name: " multiAliasName
-            echo "# ----------------------------------------\n# $multiAliasName navigation shortcuts\n# ----------------------------------------" > $HOME/.zshrc.d/$multiAliasName.zsh
-            echo "\nfunction $multiAliasName {\n\tcase \$1 in" >> $HOME/.zshrc.d/$multiAliasName.zsh
-            exitFlag="n"
-            while [ $exitFlag = "n" ]
-            do
-                read "?Alias: " aliasName
-                read "?Path: " aliasPath
-                echo "\t\t\"$aliasName\") eval \"cd $aliasPath\"" >> $HOME/.zshrc.d/$multiAliasName.zsh
-                echo "\t\t;;" >> $HOME/.zshrc.d/$multiAliasName.zsh
-                read "?End multi alias script creation? (y/n) " exitFlag
-            done
-            echo "\tesac\n}" >> $HOME/.zshrc.d/$multiAliasName.zsh
-            eval "zshrc -r"
+            __zshrc_multialias
             ;;
         ("-l")
-            for cfg in ~/.zshrc.d/*.zsh; do
-                echo ${$(basename -- "$cfg")%.*}
-            done
+            __zshrc_list
             ;;
         ("-u")
-            echo "Updating zsh script..."
-            rm -f "$HOME"/.zshrc.d/zsh.zsh
-            eval "curl -s https://raw.githubusercontent.com/thoga31/zshrc/master/.zshrc.d/zsh.zsh -o "$HOME"/.zshrc.d/zsh.zsh"
-            eval "zshrc -r"
-            echo "zsh script updated"
+            __zshrc_update
             ;;
         ("-h")
-            zshrc_help
+            __zshrc_help
             ;;
         (*)
-            eval "zshrc -h"
+            echo "   $(font fg_red)Invalid arguments.$(font reset)"
+            echo "   Here's some help :)\n"
+            __zshrc_help
             ;;
     esac
 }
