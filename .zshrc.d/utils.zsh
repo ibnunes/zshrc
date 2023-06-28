b4a409f509ee421631612809adb30e535ff5167c
Igor Nunes <thoga31@gmail.com>
2023-06-28 03:21:25 +0100

    Adds yes/no function to utils

--- .zshrc.d/utils.zsh
+++ .zshrc.d/utils.zsh
@@ -117,3 +117,12 @@ function font() {
     PARAM=$(IFS=';' ; echo "${^PARAM}")
     echo -n $ESCAPE | sed -r -e "s/§/$PARAM/"
 }
+
+function yesno() {
+    local answer=
+    while [[ ! $answer =~ "y|Y|n|N" ]]; do
+        echo -n "$@ (y/N) " > /dev/stdout
+        read answer
+    done
+    [[ $answer =~ 'y|Y' ]] && return 0 || return 1      # [0]=Yes [1]=No
+}
